24f0f1385b3a068523aac642a839fb91e4fcfafb
Bashir Abubakar <bashops1@gmail.com>
2026-06-29 09:08:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from analytics import analytics_summary, track_analysis, update_pipeline_status, generate_pitch
 from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
